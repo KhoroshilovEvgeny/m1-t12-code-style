540fb1a46fa846e1c9918d17443d0f2681ac5676
yelgazin <yelgazin@yandex.ru>
2023-07-03 23:03:05 +0300

    Добавить комментарии

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class DepositCalculator {
     double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
-        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod); // Лучше без дополнительных скобок,
+        // т.к. снижается читаемость кода
         return roundToScale(pay, 2);
     }
 
-    double calculateSimplePercent(double amount,double yearRate, int depositPeriod) {
+    double calculateSimplePercent(double amount,double yearRate, int depositPeriod) { // Лучше ставить пробел после ,
          return roundToScale(amount + amount * yearRate * depositPeriod, 2);
     }
 
-    double roundToScale(double value, int places) {
+    double roundToScale(double value, int places) { // Отличный выбор имени функции
         double scale = Math.pow(10, places);
         return Math.round(value * scale) / scale;
     }
 
     void calculateDepositWithPercent() {
-        int period;
+        int period; // Лучше определять локальные переменные перед использованием
         int action;
 
         Scanner scanner = new Scanner(System.in);
@@ -26,13 +27,14 @@ public class DepositCalculator {
         period = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double depositWithPercent = 0;
+        double depositWithPercent = 0; // Отличный выбор имени переменной
         if (action == 1) {
             depositWithPercent = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
             depositWithPercent = calculateComplexPercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + depositWithPercent);
+        // Строка не влезыет в требуемый диапазон 80 символов
     }
 
     public static void main(String[] args) {
